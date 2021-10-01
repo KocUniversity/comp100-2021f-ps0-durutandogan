3d3b06900d8c0b61ca2ca5c138d3cdaa9e4e1130
durutandogan <91738899+durutandogan@users.noreply.github.com>
2021-10-01 13:44:02 +0000

    finish code

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print('hello')
-print('hello2')
-
-
+import numpy
+print('hello!')
+print('enter number x')
+x=input()
+print('enter number y')
+y=input()
+x=int(x)
+y=int(y)
+print(x**y)
+log=numpy.log2
+print(log(x))
+print('0079479')
